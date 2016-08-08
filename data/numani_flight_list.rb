24b115840aed5677ada6b9b35e6ea8bc06f7c533
Kim Cottrell <kim@leftplusright.com>
2016-08-09 05:07:56 +0700

    added in numani flight list

---
+++ data/numani_flight_list.rb
@@ -0,0 +1,220 @@
+# numani flight list
+
+flight_hash = [
+  {
+    time: '20:00',
+    origin: 'SEOUL',
+    flight_abbr: 'HG',
+    flight_num: '0614',
+    remarks: 'ON TIME',
+    is_blizzard: true,
+    current_map: false
+  },
+  {
+    time: '21:30',
+    origin: 'MOSCOW',
+    flight_abbr: 'ES',
+    flight_num: '4R41',
+    remarks: 'CANCELED',
+    is_blizzard: false,
+    current_map: false
+  },
+  {
+    time: '22:15',
+    origin: 'CORK',
+    flight_abbr: 'BF',
+    flight_num: '3264',
+    remarks: 'ON TIME',
+    is_blizzard: true,
+    current_map: false
+  },
+  {
+    time: '22:55',
+    origin: 'HONG KONG',
+    flight_abbr: 'HA',
+    flight_num: '3252',
+    remarks: 'ON TIME',
+    is_blizzard: false,
+    current_map: false
+  },
+  {
+    time: '24:00',
+    origin: 'BUENOS AIRES',
+    flight_abbr: 'OL',
+    flight_num: '7215',
+    remarks: 'ON TIME',
+    is_blizzard: false,
+    current_map: false
+  },
+  {
+    time: '1:05',
+    origin: 'ROME',
+    flight_abbr: 'VW',
+    flight_num: 'A353',
+    remarks: 'ON TIME',
+    is_blizzard: false,
+    current_map: false
+  },
+  {
+    time: '1:20',
+    origin: 'MADRID',
+    flight_abbr: 'CQ',
+    flight_num: '4142',
+    remarks: 'ON TIME',
+    is_blizzard: false,
+    current_map: false
+  },
+  {
+    time: '2:25',
+    origin: 'PARIS',
+    flight_abbr: 'SJ',
+    flight_num: '5863',
+    remarks: 'ON TIME',
+    is_blizzard: false,
+    current_map: false
+  },
+  {
+    time: '2:35',
+    origin: 'HANAMURA',
+    flight_abbr: 'KU',
+    flight_num: '4352',
+    remarks: 'ON TIME',
+    is_blizzard: false,
+    current_map: true
+  },
+  {
+    time: '2:55',
+    origin: 'DELHI',
+    flight_abbr: 'PO',
+    flight_num: '2567',
+    remarks: 'ON TIME',
+    is_blizzard: false,
+    current_map: false
+  },
+  {
+    time: '3:00',
+    origin: 'SHANGHAI',
+    flight_abbr: 'RG',
+    flight_num: '2554',
+    remarks: 'ON TIME',
+    is_blizzard: true,
+    current_map: false
+  },
+  {
+    time: '3:55',
+    origin: 'FRANKFURT',
+    flight_abbr: 'JG',
+    flight_num: '2564',
+    remarks: 'CANCELED',
+    is_blizzard: false,
+    current_map: false
+  },
+  {
+    time: '4:00',
+    origin: 'SINGAPORE',
+    flight_abbr: 'GR',
+    flight_num: '0254',
+    remarks: 'ON TIME',
+    is_blizzard: true,
+    current_map: false
+  },
+  {
+    time: '4:20',
+    origin: 'AUSTIN',
+    flight_abbr: 'TE',
+    flight_num: '1331',
+    remarks: 'CANCELED',
+    is_blizzard: true,
+    current_map: false
+  },
+  {
+    time: '4:10',
+    origin: 'DORADO',
+    flight_abbr: 'SX',
+    flight_num: '0126',
+    remarks: 'ON TIME',
+    is_blizzard: false,
+    current_map: true
+  },
+  {
+    time: '4:45',
+    origin: 'NEW YORK',
+    flight_abbr: 'EE',
+    flight_num: '2342',
+    remarks: 'ON TIME',
+    is_blizzard: false,
+    current_map: false
+  },
+  {
+    time: '5:00',
+    origin: 'SAN FRANCISCO',
+    flight_abbr: 'UT',
+    flight_num: '2543',
+    remarks: 'ON TIME',
+    is_blizzard: false,
+    current_map: false
+  },
+  {
+    time: '5:20',
+    origin: 'TAIPEI',
+    flight_abbr: 'KE',
+    flight_num: '7145',
+    remarks: 'ON TIME',
+    is_blizzard: true,
+    current_map: false
+  },
+  {
+    time: '5:35',
+    origin: 'JOHANNESBURG',
+    flight_abbr: 'DQ',
+    flight_num: '1276',
+    remarks: 'ON TIME',
+    is_blizzard: false,
+    current_map: false
+  },
+  {
+    time: '5:55',
+    origin: 'SYDNEY',
+    flight_abbr: 'AH',
+    flight_num: '8343',
+    remarks: 'ON TIME',
+    is_blizzard: true,
+    current_map: false
+  },
+  {
+    time: '6:30',
+    origin: "KING'S ROW",
+    flight_abbr: 'CK',
+    flight_num: '1117',
+    remarks: 'ON TIME',
+    is_blizzard: false,
+    current_map: true
+  },
+  {
+    time: '6:40',
+    origin: 'VERSAILLES',
+    flight_abbr: 'GR',
+    flight_num: '3337',
+    remarks: 'ON TIME',
+    is_blizzard: true,
+    current_map: false
+  },
+  {
+    time: '6:55',
+    origin: 'LOS ANGELES',
+    flight_abbr: 'AT',
+    flight_num: '3245',
+    remarks: 'ON TIME',
+    is_blizzard: false,
+    current_map: false
+  },
+  {
+    time: '7:20',
+    origin: 'IRVINE',
+    flight_abbr: 'BJ',
+    flight_num: '3567',
+    remarks: 'DELAYED',
+    is_blizzard: true,
+    current_map: false
+  }
+]
