f0c9a9f96241a06afc7edbd8882da77811fa7c95
Kim <kimdcottrell@gmail.com>
2016-08-10 03:46:26 +0700

    added bitmapper class and example (#1)

---
+++ class/bitmapper.rb
@@ -0,0 +1,24 @@
+require "Base64"
+require "rmagick"
+include Magick
+
+class Bitmapper
+  def self.convert(input_string, output_file="./dump.gif")
+    bin = ""
+    input_string.each_byte { |n| bin << "%08b" % n }
+    size = Math.sqrt(bin.length).to_i
+    img = Image.new(size, size)
+    draw = Draw.new
+    draw.pointsize = 1
+    (0..size).each do |x|
+      (0..size).each do |y|
+        draw.point(x,y) if bin[x*size+y] == "1"
+      end
+    end
+    draw.draw(img)
+    if img.write(output_file)
+      return output_file
+    end
+    return false
+  end
+end
